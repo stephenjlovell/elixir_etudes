84aae591dac42cac4ca7bbc74c3933c6e9d61a48
Stephen Lovell <sjlovell34@gmail.com>
2015-08-09 18:53:59 -0400

    add guard clauses

--- geometry.ex
+++ geometry.ex
@@ -6,15 +6,15 @@ defmodule Geom do
   @doc """
     This function calculates the area of the specified shape.
   """
-  def area(:rectangle, height, width) do
+  def area(:rectangle, height, width) when height > 0 and width > 0 do
     height * width
   end
 
-  def area(:triangle, height, width) do
+  def area(:triangle, height, width) when height > 0 and width > 0 do
     (height * width) / 2.0
   end
 
-  def area(:ellipse, major_radius, minor_radius) do
+  def area(:ellipse, major_radius, minor_radius) when major_radius > 0 and minor_radius > 0  do
     :math.pi() * major_radius * minor_radius
   end
 
