9fe9d3cf54580397e2354316db137facf5a59ed3
Stephen Lovell <sjlovell34@gmail.com>
2015-12-30 14:39:24 -0500

    finish 4.1

--- geometry.ex
+++ geometry.ex
@@ -6,22 +6,13 @@ defmodule Geom do
   @doc """
     This function calculates the area of the specified shape.
   """
-  def area({shape, dim1, dim2}) do
-    area(shape, dim1, dim2)
+  @spec area(atom(), number(), number()) :: number()
+  def area(shape, dim1, dim2) when dim1 > 0 and dim2 > 0 do
+    case shape do
+      :rectangle  -> dim1 * dim2
+      :triangle   -> (dim1 * dim2) / 2
+      :elipse     -> :math.pi() * dim1 * dim2
+    end
   end
-
-  defp area(:rectangle, height, width) when height > 0 and width > 0 do
-    height * width
-  end
-
-  defp area(:triangle, height, width) when height > 0 and width > 0 do
-    (height * width) / 2.0
-  end
-
-  defp area(:ellipse, major_radius, minor_radius) when major_radius > 0 and minor_radius > 0  do
-    :math.pi() * major_radius * minor_radius
-  end
-
-  defp area(_, _, _), do: 0  # example purposes only.  You'd really want to raise an exception.
 
 end
