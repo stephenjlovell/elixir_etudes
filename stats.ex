db41394a43f6df956a87a9f8b4273ccab7fb3552
Stephen Lovell <sjlovell34@gmail.com>
2016-01-14 18:08:38 -0500

    8.3 using foldl

--- stats.ex
+++ stats.ex
@@ -30,4 +30,47 @@ defmodule Stats do
     [ minimum(nums), maximum(nums) ]
   end
 
+  @doc "Returns the mean (average) of a list of numbers"
+  @spec mean([number]) :: number
+  def mean(nums) do
+    sum(nums) / Enum.count(nums)
+  end
+
+  @doc "Returns the sum of a list of numbers"
+  @spec sum([number]) :: number
+  def sum(nums), do: List.foldl(nums, 0, fn x, acc -> x + acc end )
+
+  @doc "calculates the Population Standard Deviation of a list of numbers."
+  @spec stdv_pop([number]) :: number
+  def stdv_pop(nums) do
+    variance_pop(nums) |> :math.sqrt()
+  end
+
+  @doc "calculates the (sample) Standard Deviation of a list of numbers."
+  @spec stdv([number]) :: number
+  def stdv(nums) do
+    variance(nums) |> :math.sqrt()
+  end
+
+  @doc "calculates the Population Variance of a list of numbers."
+  @spec variance([number]) :: number
+  def variance(nums) do
+    variance(nums, Enum.count(nums)-1)
+  end
+
+  @doc "calculates the (sample) Variance of a list of numbers."
+  @spec variance_pop([number]) :: number
+  def variance_pop(nums) do
+    variance(nums, Enum.count(nums))
+  end
+
+  defp variance(nums, n) do
+    mu = mean(nums)
+    sum(for x <- nums do
+      delta = x - mu
+      delta * delta
+    end) / n
+  end
+
+
 end
