238179760644549a11d1d63c47b236b14230d69f
Stephen Lovell <sjlovell34@gmail.com>
2016-01-19 17:22:33 -0500

    8.6 explaining an algo

--- cards.ex
+++ cards.ex
@@ -9,5 +9,25 @@ defmodule Cards do
     end
   end
 
+  @doc "shuffles the deck of cards into a random order."
+  @spec shuffle([tuple]) :: [tuple]
+  def shuffle(list) do
+    :random.seed(:erlang.now())
+    shuffle(list, [])
+  end
+
+  @doc "returns the shuffled cards."
+  defp shuffle([], shuffled) do
+    shuffled
+  end
+
+  @doc """
+    shuffle/2 takes two lists representing shuffled and unshuffled cards.  It recursively draws a card at random from the unshuffled
+    cards and adds it to the shuffled cards, until no unshuffled cards remain.  This is known as the Knuth Shuffle.
+  """
+  defp shuffle(unshuffled, shuffled) do
+    {leading, [h | t]} = Enum.split(unshuffled, :random.uniform(Enum.count(unshuffled)) - 1)
+    shuffle(leading ++ t, [h | shuffled])
+  end
 
 end
